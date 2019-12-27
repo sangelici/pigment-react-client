512d0c0412490973b9cb31d2cf425a6025b0446d
sangelici <sangelici94@gmail.com>
2019-12-27 16:07:27 -0500

    Adjust styling
    - remove container from component
    - adjust font sizes

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,19 +1,16 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
 import Nav from 'react-bootstrap/Nav'
 import './Stylesheet.scss'
 
 const Homepage = props => (
-  <Container fluid={true}>
-    <div className="drop-shadow">
-      <div className="glass"></div>
-      <span className="main-title">PIGMENT</span>
-      <span className="links">
-        <Nav.Link className="acct-link" href="#sign-up">Sign Up</Nav.Link>
-        <Nav.Link className="acct-link" href="#sign-in">Sign In</Nav.Link>
-      </span>
-    </div>
-  </Container>
+  <div className="drop-shadow">
+    <div className="glass"></div>
+    <span className="main-title">PIGMENT</span>
+    <span className="links">
+      <Nav.Link className="acct-link" href="#sign-up">Sign Up</Nav.Link>
+      <Nav.Link className="acct-link" href="#sign-in">Sign In</Nav.Link>
+    </span>
+  </div>
 )
 
 export default Homepage
